721fd696475ec8fa963b344924f58ee979c8e500
Xal Tabba <neoaikon1985@gmail.com>
2024-07-14 14:20:34 -0400

    Initial commit, working with Patch 4

---
+++ Scripts/SLZ.Marrow/SLZ.Marrow/LateReferences/IgnoreCollidersSceneObjectLateReference.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace SLZ.Marrow.LateReferences
+{
+	[Serializable]
+	public class IgnoreCollidersSceneObjectLateReference : LateReference<IgnoreCollidersSceneObject>
+	{
+	}
+}
---
+++ Scripts/SLZ.Marrow/SLZ.Marrow/Zones/FilteredDespawn.cs
@@ -0,0 +1,10 @@
+using SLZ.Marrow.Interaction;
+using UnityEngine;
+
+namespace SLZ.Marrow.Zones
+{
+	[RequireComponent(typeof(Zone))]
+	public class FilteredDespawn : ZoneItem
+	{
+	}
+}
